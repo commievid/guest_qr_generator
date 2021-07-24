728cc43e2c510e3d56ef3ca6fb2ca4266f372264
David Nguyen <commievid@hotmail.com>
2021-07-24 22:09:37 +0100

    Tweaks to Selenium router config
    Script now logs out explicitly, and adds support for a virtual display for headerless running.
    Copied over the Waveshare EPD library code in case that is simpler to use in that manner.

--- src/gqrg/webrouter.py
+++ src/gqrg/webrouter.py
@@ -1,16 +1,27 @@
+from sys import platform
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import keyring
+from pyvirtualdisplay import Display
 
 # pip install selenium
 # pip install keyring
+# pip install pyvirtualdisplay
 
-def update_password(ssid_key):
+def update_password(ssid_key, headless = True):
+    
+    if headless:
+        print("Starting virtual display")
+        disp = Display()
+        disp.start()
+
+    print("Starting chromedriver")
     driver = webdriver.Chrome()
 
+    print("Connecting to router")
     # Password will need setting first: keyring.set_password
     driver.get("http://admin:" + keyring.get_password("gqrg", "admin") + "@192.168.1.1")
 
@@ -31,5 +42,20 @@ def update_password(ssid_key):
 
     time.sleep(5)
 
+    print("Logging out")
+    driver.switch_to.default_content()
+    driver.switch_to.frame("topframe")      
+
+    logout = wait.until(EC.visibility_of_element_located((By.ID, "logout")))
+    logout.click()
+
+    time.sleep(5)
+
+    print("Stopping chromedriver")
     driver.close()
+
+    if headless:
+        print("Stopping virtual display")
+        disp.stop()
+        # display is stopped
     
