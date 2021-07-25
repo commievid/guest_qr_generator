88bee5e6f88d7fe9690f501fd5b3d3ee65d6bdfe
David Nguyen <dnguyen@digital.homeoffice.gov.uk>
2021-07-25 08:06:53 +0100

    Convert to bmp and resize

--- src/gqrg/epaper.py
+++ src/gqrg/epaper.py
@@ -1,5 +1,5 @@
 import epd4in2
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import logging
 import time
 
@@ -13,9 +13,19 @@ def draw_qr():
     epd.init()
     epd.Clear()
 
-    logging.info("Reading in the PNG file")
-    qrImage = Image.open("qr.png")
-    epd.display(epd.getbuffer(qrImage))
+    logging.info("Reading in the PNG file and saving to BMP")
+    Image.open("qr.png").save("qr.bmp")
+    qrImage = Image.open("qr.bmp")
+    qrImage.resize((300, 300))
+
+    resultingImage = Image.new("1", (epd.height, epd.width), 255)
+    resultingImage.paste(qrImage, (50, 0))
+
+    font18 = ImageFont.truetype("Font.ttc", 18)
+    draw = ImageDraw.Draw(resultingImage)
+    draw.text((0,0), "Test!", font = font18, fill = 0)
+
+    epd.display(epd.getbuffer(resultingImage))
 
     time.sleep(2)
     logging.info("Send sleep to e-Paper")
